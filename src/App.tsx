132a1f1bd241e91d1177651eb00eb57b791ad39e
leonooo13 <lidefree@qq.com>
2024-08-18 23:20:58 +0800

    add resume editor

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,18 @@ import Login from './pages/Login';
 import Article from './pages/Article';
 import MarkdownEditor from './components/MarkdownViewer';
 import Profile from './pages/Profile';
+import ResumeEditor from './pages/ResumeEditor';
 import './App.css'; // 如果有全局样式的话
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<MarkdownEditor />} />
+        <Route path="/markdown" element={<MarkdownEditor />} />
         <Route path="/article" element={<Article />} />
-        <Route path="/markdown" element={<Login />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path='/' element = {<ResumeEditor/>}/>
       </Routes>
     </Router>
   );
